049b9ae578a16d8730bf8e7f57019eec585cf4ad
FadQode <fadhilerdyaqasmal@gmail.com>
2022-12-14 15:03:29 +0700

    added highscore

--- scoreboard.py
+++ scoreboard.py
@@ -2,10 +2,15 @@ from turtle import Turtle
 Font = ("Courier", 15, "normal")
 
 
+
+
+
 class Score(Turtle):
     def __init__(self):
         super().__init__()
-        self.highscore = 0
+        with open("highscore", mode="r") as file:
+            a = file.read()
+        self.highscore = int(a)
         self.score = 0
         self.color("white")
         self.penup()
@@ -15,6 +20,8 @@ class Score(Turtle):
     def highestscore(self):
         if self.score > self.highscore:
             self.highscore = self.score
+        with open("highscore", mode="w") as file:
+            file.write(str(self.highscore))
         self.score = 0
         self.clear()
         self.write(align="center", arg=f"score: {self.score}   highscore: {self.highscore}", font=Font)
@@ -24,3 +31,4 @@ class Score(Turtle):
         self.clear()
         self.score += 1
         self.write(align="center", arg=f"score: {self.score}   highscore: {self.highscore}", font=Font)
+
